1710f00359a2db51f626e95b0ad827a773bd6d3e
Komyk <o.kolmyk@mail.ru>
2016-09-07 19:15:05 +0300

    Добавлена таблица material_type добавлены контролер MaterialTypeController.php добавлена модель MaterialType.php добавлена вьюха material_type

---
+++ migrations/m160902_161051_material_type_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160902_161051_material_type_table extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m160902_161051_material_type_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
