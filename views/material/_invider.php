e35189f79911549b7934e2db2af93edc651ed629
Vladymyr Protsenko <grayfolk@gmail.com>
2016-10-09 02:57:31 +0300

    Improve materials

--- views/material/_invider.php
+++ views/material/_invider.php
@@ -11,11 +11,34 @@
     <?php if($location->type_id == 2):?>
     <tr>
       <td><?= $location->title?></td>
-      <td><?php $m = []; foreach($materials as $material):?>
-        <?php if(array_key_exists($location->id, $locationsArray) && in_array($material->id, $locationsArray[$location->id])) $m[] = $material->title;?>
-        <?php endforeach;?>
-        <?= implode(', ',$m)?>
-        </td>
+      <td><div class="row">
+          <?php $m = []; foreach($materials as $material):?>
+          <?php if(array_key_exists($location->id, $locationsArray) && in_array($material->id, $locationsArray[$location->id])) $m[$material->type_id][] = $material->title;?>
+          <?php endforeach;?>
+          <?php foreach($m as $type=>$list):?>
+          <?php if(count($list)):?>
+          <div class="col-md-2">
+            <?php
+        switch($type){
+			case 3:
+				echo ' <span class="label label-primary">Материалы:</span> ';
+				break;
+			case 2:
+				echo ' <span class="label label-primary">Камни:</span> ';
+				break;
+			case 1:
+				echo ' <span class="label label-primary">Обычные материалы:</span> ';
+				break;
+			default: break;
+		}
+		?>
+          </div>
+          <div class="col-md-10">
+            <?= implode(', ',$list) . '<div class="clearfix"></div>'?>
+          </div>
+          <?php endif;?>
+          <?php endforeach;?>
+        </div></td>
     </tr>
     <?php endif;?>
     <?php endforeach;?>
--- views/material/_regular.php
+++ views/material/_regular.php
@@ -11,11 +11,31 @@
     <?php if($material->type_id == 1):?>
     <tr>
       <td><?= $material->title?></td>
-      <td><?php $l = []; foreach($locations as $location):?>
-        <?php if(array_key_exists($material->id, $materialsArray) && in_array($location->id, $materialsArray[$material->id])) $l[] = $location->title;?>
-        <?php endforeach;?>
-        <?= implode(', ',$l)?>
-        </td>
+      <td><div class="row">
+          <?php $l = []; foreach($locations as $location):?>
+          <?php if(array_key_exists($material->id, $materialsArray) && in_array($location->id, $materialsArray[$material->id])) $l[$location->type_id][] = $location->title;?>
+          <?php endforeach;?>
+          <?php foreach($l as $type=>$list):?>
+          <?php if(count($list)):?>
+          <div class="col-md-2">
+            <?php
+        switch($type){
+			case 2:
+				echo ' <span class="label label-primary">Захватчики:</span> ';
+				break;
+			case 1:
+				echo ' <span class="label label-primary">Локации:</span> ';
+				break;
+			default: break;
+		}
+		?>
+          </div>
+          <div class="col-md-10">
+            <?= implode(', ',$list) . '<div class="clearfix"></div>'?>
+          </div>
+          <?php endif;?>
+          <?php endforeach;?>
+        </div></td>
     </tr>
     <?php endif;?>
     <?php endforeach;?>
